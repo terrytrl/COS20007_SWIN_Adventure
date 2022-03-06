79d71dc8da6f95308aeb91c4265d4bcddbc7dcaa
terrytrl <73771668+terrytrl@users.noreply.github.com>
2022-03-06 21:39:03 +1100

    Update

---
+++ IHaveInventory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace SwinAdventure
+{
+    public interface IHaveInventory
+    {
+        GameObject Locate(string id);
+        string Name { get; }
+
+    }
+}
